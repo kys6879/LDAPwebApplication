3493d35a7cc7d60372e7672a837a14a4f810e695
kys6879 <kys6879@naver.com>
2018-07-26 12:30:44 +0900

    dTree 라이브러리 연결 /monitor

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var dTree = require('../library/dTree/dtree');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('monitoring');
+  res.render('monitor');
 });
 
 module.exports = router;
