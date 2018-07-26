8f3c9ab44603f2ec7d7c16e5e5b7aae8cbfd24b9
kys6879 <kys6879@naver.com>
2018-07-26 11:55:29 +0900

    도커파일생성

---
+++ dockerFiles/open_ldapadmin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+docker run --name ldap-service --hostname ldap-service --detach osixia/openldap:1.1.8
+docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host --detach osixia/phpldapadmin:0.7.1
+
+PHPLDAP_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" phpldapadmin-service)
+
+echo "Go to: https://$PHPLDAP_IP"
+echo "Login DN: cn=admin,dc=example,dc=org"
+echo "Password: admin"
