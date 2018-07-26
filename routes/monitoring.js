6a00bc1710855ceeb58f61b157a2f14dd9360658
kys6879 <kys6879@naver.com>
2018-07-26 20:45:44 +0900

    entries 객체 EJS 전달

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -1,11 +1,24 @@
 var express = require('express');
+const ldap_get_all = require('../library/ldap_get_all');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('monitor',{
-    result : 1
-  });
+  let filter = "(ObjectClass=*)";
+  ldap_get_all.getAllRecords(filter).then((results)=>{
+    console.log("검색성공! : " +results.entries[0]);
+    let entriesstr = JSON.stringify(results.entries);
+    res.render('monitor',{
+      results : results,
+      entriesstr : entriesstr,
+      r : 1
+    })
+  },(err)=>{
+    console.log("검색실패",err);
+    res.send("검색실패");
+  }
+  )
 });
+
 
 module.exports = router;
