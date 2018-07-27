7e5eb0ef8cbcfd6be000c567cac13679138a9509
kys6879 <kys6879@naver.com>
2018-07-27 15:01:05 +0900

    어드민상세보기

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const ldap_get_all = require('../library/ldap_get_all');
+const config = require('../config/config');
+const ldap_get_admin = require('../library/ldap_get_admin');
 const router = express.Router();
 
 /* GET home page. */
@@ -7,10 +8,21 @@ router.get('/', function(req, res, next) {
   res.send('admin page ');
 });
 
-router.get('/:cn', function(req, res, next) {
-  let cn = req.params.cn;
-  res.send(`${cn}`);
-});
+// 특정 어드민 상세보기
+router.get('/:cn',(request,response,next)=>{
+  let cn = request.params.cn;
+  let filter = "(ObjectClass=organizationalRole)";
+  let baseDn = `cn=${cn},${config.adSuffix}`
+  ldap_get_admin.getAdmin(baseDn,filter).then((results) => {
+    console.log("검색성공!" + results);
+    response.render('admin_detail',{
+      entry : results.entries[0]
+    })
+  }, (err)=>{
+      console.log("검색실패",err);
+      response.send("검색실패");
+  })
+}); 
 
 
 module.exports = router;
