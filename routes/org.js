2103190c12160a4ec10dc8adb8f1379e99e93148
kys6879 <kys6879@naver.com>
2018-07-27 15:10:44 +0900

    최상위조직상세보기

--- routes/org.js
+++ routes/org.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const ldap_get_all = require('../library/ldap_get_all');
+const config = require('../config/config');
+const ldap_get_org = require('../library/ldap_get_org');
 const router = express.Router();
 
 router.get('/',(request,response,next)=>{
   response.send("org");
 }); 
 
+// 특정 어드민 상세보기
 router.get('/:dc',(request,response,next)=>{
   let dc = request.params.dc;
-  response.send(""+dc);
+  let filter = "(ObjectClass=organization)";
+  let baseDn = `${config.adSuffix}`
+  ldap_get_org.getOrg(baseDn,filter).then((results) => {
+    console.log("검색성공!" + results);
+    response.render('org_detail',{
+      entry : results.entries[0]
+    })
+  }, (err)=>{
+      console.log("검색실패",err);
+      response.send("검색실패");
+  })
 }); 
 
 module.exports = router;
